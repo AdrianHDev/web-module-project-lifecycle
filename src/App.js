f63c190fb0fff6d662fa4484b9e0bb95858d0c00
AdrianHDev <adrianhensondev@gmail.com>
2021-06-01 16:09:38 -0400

    write up basic usercard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,218 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Usercard from "./Components/Usercard";
+import styled from "styled-components";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const StyledPage = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  overflow: auto;
+  minheight: 100%;
+  background: #b4aee8;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const initialState = {
+  user: {
+    avatar_url: "https://avatars.githubusercontent.com/u/13549227?v=4",
+    bio: "Yep, that's me!",
+    blog: "",
+    company: null,
+    created_at: "2015-07-29T03:53:23Z",
+    email: null,
+    events_url: "https://api.github.com/users/AdrianHDev/events{/privacy}",
+    followers: 6,
+    followers_url: "https://api.github.com/users/AdrianHDev/followers",
+    following: 13,
+    following_url:
+      "https://api.github.com/users/AdrianHDev/following{/other_user}",
+    gists_url: "https://api.github.com/users/AdrianHDev/gists{/gist_id}",
+    gravatar_id: "",
+    hireable: null,
+    html_url: "https://github.com/AdrianHDev",
+    id: 13549227,
+    location: "Saint Thomas, PA",
+    login: "AdrianHDev",
+    name: "Adrian Henson",
+    node_id: "MDQ6VXNlcjEzNTQ5MjI3",
+    organizations_url: "https://api.github.com/users/AdrianHDev/orgs",
+    public_gists: 1,
+    public_repos: 40,
+    received_events_url:
+      "https://api.github.com/users/AdrianHDev/received_events",
+    repos_url: "https://api.github.com/users/AdrianHDev/repos",
+    site_admin: false,
+    starred_url:
+      "https://api.github.com/users/AdrianHDev/starred{/owner}{/repo}",
+    subscriptions_url: "https://api.github.com/users/AdrianHDev/subscriptions",
+    twitter_username: null,
+    type: "User",
+    updated_at: "2021-05-31T17:08:53Z",
+    url: "https://api.github.com/users/AdrianHDev",
+  },
+  followers: [
+    {
+      login: "spotlightishere",
+      id: 10055256,
+      node_id: "MDQ6VXNlcjEwMDU1MjU2",
+      avatar_url: "https://avatars.githubusercontent.com/u/10055256?v=4",
+      gravatar_id: "",
+      url: "https://api.github.com/users/spotlightishere",
+      html_url: "https://github.com/spotlightishere",
+      followers_url: "https://api.github.com/users/spotlightishere/followers",
+      following_url:
+        "https://api.github.com/users/spotlightishere/following{/other_user}",
+      gists_url: "https://api.github.com/users/spotlightishere/gists{/gist_id}",
+      starred_url:
+        "https://api.github.com/users/spotlightishere/starred{/owner}{/repo}",
+      subscriptions_url:
+        "https://api.github.com/users/spotlightishere/subscriptions",
+      organizations_url: "https://api.github.com/users/spotlightishere/orgs",
+      repos_url: "https://api.github.com/users/spotlightishere/repos",
+      events_url:
+        "https://api.github.com/users/spotlightishere/events{/privacy}",
+      received_events_url:
+        "https://api.github.com/users/spotlightishere/received_events",
+      type: "User",
+      site_admin: false,
+    },
+    {
+      login: "EastAPOLO",
+      id: 12688094,
+      node_id: "MDQ6VXNlcjEyNjg4MDk0",
+      avatar_url: "https://avatars.githubusercontent.com/u/12688094?v=4",
+      gravatar_id: "",
+      url: "https://api.github.com/users/EastAPOLO",
+      html_url: "https://github.com/EastAPOLO",
+      followers_url: "https://api.github.com/users/EastAPOLO/followers",
+      following_url:
+        "https://api.github.com/users/EastAPOLO/following{/other_user}",
+      gists_url: "https://api.github.com/users/EastAPOLO/gists{/gist_id}",
+      starred_url:
+        "https://api.github.com/users/EastAPOLO/starred{/owner}{/repo}",
+      subscriptions_url: "https://api.github.com/users/EastAPOLO/subscriptions",
+      organizations_url: "https://api.github.com/users/EastAPOLO/orgs",
+      repos_url: "https://api.github.com/users/EastAPOLO/repos",
+      events_url: "https://api.github.com/users/EastAPOLO/events{/privacy}",
+      received_events_url:
+        "https://api.github.com/users/EastAPOLO/received_events",
+      type: "User",
+      site_admin: false,
+    },
+    {
+      login: "mastercode10",
+      id: 36807046,
+      node_id: "MDQ6VXNlcjM2ODA3MDQ2",
+      avatar_url: "https://avatars.githubusercontent.com/u/36807046?v=4",
+      gravatar_id: "",
+      url: "https://api.github.com/users/mastercode10",
+      html_url: "https://github.com/mastercode10",
+      followers_url: "https://api.github.com/users/mastercode10/followers",
+      following_url:
+        "https://api.github.com/users/mastercode10/following{/other_user}",
+      gists_url: "https://api.github.com/users/mastercode10/gists{/gist_id}",
+      starred_url:
+        "https://api.github.com/users/mastercode10/starred{/owner}{/repo}",
+      subscriptions_url:
+        "https://api.github.com/users/mastercode10/subscriptions",
+      organizations_url: "https://api.github.com/users/mastercode10/orgs",
+      repos_url: "https://api.github.com/users/mastercode10/repos",
+      events_url: "https://api.github.com/users/mastercode10/events{/privacy}",
+      received_events_url:
+        "https://api.github.com/users/mastercode10/received_events",
+      type: "User",
+      site_admin: false,
+    },
+    {
+      login: "banishlight",
+      id: 8949740,
+      node_id: "MDQ6VXNlcjg5NDk3NDA=",
+      avatar_url: "https://avatars.githubusercontent.com/u/8949740?v=4",
+      gravatar_id: "",
+      url: "https://api.github.com/users/banishlight",
+      html_url: "https://github.com/banishlight",
+      followers_url: "https://api.github.com/users/banishlight/followers",
+      following_url:
+        "https://api.github.com/users/banishlight/following{/other_user}",
+      gists_url: "https://api.github.com/users/banishlight/gists{/gist_id}",
+      starred_url:
+        "https://api.github.com/users/banishlight/starred{/owner}{/repo}",
+      subscriptions_url:
+        "https://api.github.com/users/banishlight/subscriptions",
+      organizations_url: "https://api.github.com/users/banishlight/orgs",
+      repos_url: "https://api.github.com/users/banishlight/repos",
+      events_url: "https://api.github.com/users/banishlight/events{/privacy}",
+      received_events_url:
+        "https://api.github.com/users/banishlight/received_events",
+      type: "User",
+      site_admin: false,
+    },
+    {
+      login: "4nickpick",
+      id: 3085512,
+      node_id: "MDQ6VXNlcjMwODU1MTI=",
+      avatar_url: "https://avatars.githubusercontent.com/u/3085512?v=4",
+      gravatar_id: "",
+      url: "https://api.github.com/users/4nickpick",
+      html_url: "https://github.com/4nickpick",
+      followers_url: "https://api.github.com/users/4nickpick/followers",
+      following_url:
+        "https://api.github.com/users/4nickpick/following{/other_user}",
+      gists_url: "https://api.github.com/users/4nickpick/gists{/gist_id}",
+      starred_url:
+        "https://api.github.com/users/4nickpick/starred{/owner}{/repo}",
+      subscriptions_url: "https://api.github.com/users/4nickpick/subscriptions",
+      organizations_url: "https://api.github.com/users/4nickpick/orgs",
+      repos_url: "https://api.github.com/users/4nickpick/repos",
+      events_url: "https://api.github.com/users/4nickpick/events{/privacy}",
+      received_events_url:
+        "https://api.github.com/users/4nickpick/received_events",
+      type: "User",
+      site_admin: false,
+    },
+    {
+      login: "ZachBosch",
+      id: 81101241,
+      node_id: "MDQ6VXNlcjgxMTAxMjQx",
+      avatar_url: "https://avatars.githubusercontent.com/u/81101241?v=4",
+      gravatar_id: "",
+      url: "https://api.github.com/users/ZachBosch",
+      html_url: "https://github.com/ZachBosch",
+      followers_url: "https://api.github.com/users/ZachBosch/followers",
+      following_url:
+        "https://api.github.com/users/ZachBosch/following{/other_user}",
+      gists_url: "https://api.github.com/users/ZachBosch/gists{/gist_id}",
+      starred_url:
+        "https://api.github.com/users/ZachBosch/starred{/owner}{/repo}",
+      subscriptions_url: "https://api.github.com/users/ZachBosch/subscriptions",
+      organizations_url: "https://api.github.com/users/ZachBosch/orgs",
+      repos_url: "https://api.github.com/users/ZachBosch/repos",
+      events_url: "https://api.github.com/users/ZachBosch/events{/privacy}",
+      received_events_url:
+        "https://api.github.com/users/ZachBosch/received_events",
+      type: "User",
+      site_admin: false,
+    },
+  ],
+};
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = initialState;
+  }
+  render() {
+    return (
+      <StyledPage>
+        <Usercard user={this.state.user} followers={this.state.followers} />
+      </StyledPage>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Usercard.js
@@ -0,0 +1,73 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledCard = styled.div`
+  background: #93329e;
+  border-radius: 80px;
+  border: 5px solid #440a67;
+  width: 79vw;
+  height: 75vh;
+  padding: 50px;
+  color: #ffe3fe;
+  style: flex;
+`;
+
+const SideBySideDiv = styled.div`
+  display: flex;
+  align-items: left;
+`;
+
+const CenteredSideBySideDiv = styled.div`
+  display: flex;
+  align-items: left;
+`;
+
+const StyledPfp = styled.img`
+  width: 12rem;
+  height: 12rem;
+  border: solid #440a67 10px;
+  border-radius: 75px;
+`;
+
+const StyledHeader = styled.h1`
+  margin: auto;
+  font-size: 5rem;
+`;
+
+const InfoDiv = styled.div`
+  margin: 10px;
+  background: #b4aee8;
+  color: #440a67;
+  padding: 8px;
+  border: 3px solid #440a67;
+  border-radius: 15px;
+`;
+
+class Usercard extends React.Component {
+  render() {
+    return (
+      <StyledCard>
+        <SideBySideDiv>
+          <StyledPfp src={this.props.user.avatar_url} />
+          <StyledHeader>{this.props.user.name}</StyledHeader>
+        </SideBySideDiv>
+        <CenteredSideBySideDiv>
+          <InfoDiv>
+            <h2>Bio:</h2>
+            <p>{this.props.user.bio}</p>
+          </InfoDiv>
+          <InfoDiv>
+            <h2>Location: </h2>
+            <p>{this.props.user.location}</p>
+          </InfoDiv>
+          <InfoDiv>
+            <h2>Public Repositories:</h2>
+            <p>{this.props.user.public_repos}</p>
+          </InfoDiv>
+        </CenteredSideBySideDiv>
+      </StyledCard>
+    );
+  }
+}
+
+export default Usercard;
